77ae4ecd0f46d3e46afa3d9da33d127b1f460137
Patrick <chodowski.patrick@gmail.com>
2023-10-11 21:49:11 +0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+
+fn main() {
+    leptos::mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.set(3);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
